cdc4ebc5b151cbed09690cee2a658b2a9756c978
Andres Puulinn <andres_puulinn@symantec.com>
2017-03-23 17:56:34 +0200

    create a perl script to lookup data from memento.data file

---
+++ memento.pl
@@ -0,0 +1,34 @@
+use strict;
+
+my $mask = '(';
+for (@ARGV) {
+	if ($mask =~ /^[+]/) {
+		die "+ detected";
+	}
+	$mask .= "$_|";
+}
+chop($mask);
+$mask .= ')';
+$mask = '^' if $mask eq ')';
+
+my @results;
+my $maxTag = 0;
+my $maxKey = 0;
+my $maxValue = 0;
+
+open(OL, 'memento.data') || die ("Can't open memento.data");
+while (<OL>) {
+	next if !m/$mask/;
+
+	my ($type, $tag, $key, $value) = split(/\t/);
+	$maxTag = length($tag) if $maxTag < length($tag);
+	$maxKey = length($key) if $maxKey < length($key);
+	$maxValue = length($value) if $maxValue < length($value);
+	push @results, $_;
+}
+close(OL);
+
+for (@results) {
+	my ($type, $tag, $key, $value) = split(/\t/);
+	printf("%*s  %*s  %s", -$maxTag, $tag, -$maxKey, $key, $value)
+}
