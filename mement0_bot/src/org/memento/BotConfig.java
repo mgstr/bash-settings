11421824ec618d499dcea2c9b84f526cdb138032
Andres Puulinn <andres.puulinn@gmail.com>
2017-04-20 22:14:40 +0300

    do't store bot api key

--- mement0_bot/src/org/memento/BotConfig.java
+++ mement0_bot/src/org/memento/BotConfig.java
@@ -4,6 +4,6 @@ package org.memento;
  * Created by Vitalii_Chistiakov on 4/20/2017.
  */
 public class BotConfig {
-    public static final String COMMANDS_TOKEN = "367287749:AAGCPT1vzmTkuz02rmW-3mWunXqhnm9Wywc";
+    public static final String COMMANDS_TOKEN = "";
     public static final String COMMANDS_USER = "mement0_bot";
 }
