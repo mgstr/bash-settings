8a28960bea0e38977a462727b5ae11f092912ed0
Vitalii_Chistiakov <vitalii_chistiakov@symantec.com>
2017-04-20 15:44:44 +0300

    java bot

---
+++ mement0_bot/src/org/memento/commands/SearchCommand.java
@@ -0,0 +1,65 @@
+package org.memento.commands;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Chat;
+import org.telegram.telegrambots.api.objects.User;
+import org.telegram.telegrambots.bots.AbsSender;
+import org.telegram.telegrambots.bots.commands.BotCommand;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.logging.BotLogger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Vitalii_Chistiakov on 4/20/2017.
+ */
+public class SearchCommand extends BotCommand {
+
+    private static final String LOGTAG = "SEARCHCOMMAND";
+
+    public SearchCommand() {
+        super("s", "search");
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
+
+        StringBuilder filters = new StringBuilder();
+        if (arguments != null && arguments.length > 0) {
+            filters.append(String.join(" ", arguments));
+        }
+        Process process;
+        String searchResult;
+        try {
+            process = Runtime.getRuntime().exec("@perl memento.pl %* | colorize");
+            if (process.waitFor() != 0) {
+                BotLogger.error(LOGTAG, "memento.pl exit code is non-zero");
+            }
+            searchResult = extractOutput(process);
+        } catch (IOException | InterruptedException e) {
+            BotLogger.error(LOGTAG, e);
+            return;
+        }
+        SendMessage answer = new SendMessage();
+        answer.setChatId(chat.getId().toString());
+        answer.setText(searchResult);
+
+        try {
+            absSender.sendMessage(answer);
+        } catch (TelegramApiException e) {
+            BotLogger.error(LOGTAG, e);
+        }
+    }
+
+    private String extractOutput(Process process) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        return sb.toString();
+    }
+}
---
+++ mement0_bot/src/org/memento/updatehandlers/CommandHandlers.java
@@ -0,0 +1,71 @@
+package org.memento.updatehandlers;
+
+import org.memento.BotConfig;
+import org.memento.commands.HelpCommand;
+import org.memento.commands.SearchCommand;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.logging.BotLogger;
+
+/**
+ * This handler mainly works with commands to demonstrate the Commands feature of the API
+ *
+ * @author Timo Schulz (Mit0x2)
+ */
+public class CommandHandlers extends TelegramLongPollingCommandBot {
+
+    public static final String LOGTAG = "COMMANDSHANDLER";
+
+    /**
+     * Constructor.
+     */
+    public CommandHandlers() {
+        register(new SearchCommand());
+        HelpCommand helpCommand = new HelpCommand(this);
+        register(helpCommand);
+
+        registerDefaultAction((absSender, message) -> {
+            SendMessage commandUnknownMessage = new SendMessage();
+            commandUnknownMessage.setChatId(message.getChatId());
+            commandUnknownMessage.setText("The command '" + message.getText() + "' is not known by this bot. Here comes some help ");
+            try {
+                absSender.sendMessage(commandUnknownMessage);
+            } catch (TelegramApiException e) {
+                BotLogger.error(LOGTAG, e);
+            }
+            helpCommand.execute(absSender, message.getFrom(), message.getChat(), new String[] {});
+        });
+    }
+
+    @Override
+    public void processNonCommandUpdate(Update update) {
+
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                SendMessage echoMessage = new SendMessage();
+                echoMessage.setChatId(message.getChatId());
+                echoMessage.setText("Hey heres your message:\n" + message.getText());
+
+                try {
+                    sendMessage(echoMessage);
+                } catch (TelegramApiException e) {
+                    BotLogger.error(LOGTAG, e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BotConfig.COMMANDS_USER;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BotConfig.COMMANDS_TOKEN;
+    }
+}
