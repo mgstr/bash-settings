6bd86484b3cade1fddca048bf0bc09d0182d5e8b
Vitalii_Chistiakov <vitalii_chistiakov@symantec.com>
2017-04-21 17:55:08 +0300

    objectstream fix

--- mement0_bot/src/main/java/org/memento/client/SocketClient.java
+++ mement0_bot/src/main/java/org/memento/client/SocketClient.java
@@ -1,15 +1,15 @@
 package org.memento.client;
 
 import com.google.common.base.Strings;
-import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.io.IOUtils;
 import org.memento.BotConfig;
 import org.telegram.telegrambots.logging.BotLogger;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by Vitalii_Chistiakov on 4/20/2017.
@@ -18,28 +18,17 @@ public class SocketClient {
     private static final String LOGTAG = "SOCKETCLIENT";
 
     public String send(String request) {
-        String response = null;
-
         String port = System.getenv("SERVER_PORT");
         if (Strings.isNullOrEmpty(port)) {
             throw new RuntimeException("SERVER_PORT environment variable not set but required");
         }
         try (
                 Socket socket = new Socket(BotConfig.SERVER_HOST, Integer.valueOf(port));
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
                 InputStream in = socket.getInputStream()
         ) {
-            out.writeObject(request);
-            try (
-                    ObjectInputStream ois =
-                            new ObjectInputStream(in)) {
-                try {
-                    response = (String) ois.readObject();
-                } catch (ClassNotFoundException e) {
-                    BotLogger.error("Exception occured while reading response", LOGTAG, e);
-                }
-            }
-            return response;
+            out.write(request);
+            return IOUtils.toString(in, StandardCharsets.UTF_8);
         } catch (IOException e) {
             BotLogger.error(LOGTAG, e);
             return null;
