51fe2986fbfa9cddccd6ae63923c6953fdbea830
Vitalii_Chistiakov <vitalii_chistiakov@symantec.com>
2017-04-21 14:06:12 +0300

    compost

--- build.bat
+++ build.bat
@@ -5,7 +5,6 @@ REM docker tag mementobackend govnocod/memento:backend_0.2
 REM docker push govnocod/memento:backend_0.2
 
 cd ../mement0_bot
-jar uvf target/memento-0.1-jar-with-dependencies.jar target/classes/*.class
 copy target/memento-0.1-jar-with-dependencies.jar docker
 cd docker
 REM docker build -t mementobot .
