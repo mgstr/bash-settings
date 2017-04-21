4df5014f5fcea92141c8461889672b6fa42f151a
Andres Puulinn <andres_puulinn@symantec.com>
2017-04-21 17:47:48 +0300

    use real container name rather than fake 0.0.0.0

--- mement0_bot/src/main/java/org/memento/BotConfig.java
+++ mement0_bot/src/main/java/org/memento/BotConfig.java
@@ -6,6 +6,6 @@ package org.memento;
 public class BotConfig {
     public static final String COMMANDS_USER = "mement0_bot";
 
-    public static final int SERVER_PORT = 80;
-    public static final String SERVER_HOST = "0.0.0.0";
+    public static final int SERVER_PORT = 42;
+    public static final String SERVER_HOST = "backend";
 }
