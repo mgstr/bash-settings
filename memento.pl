4dfe57d51d08e870d04c7d1fbc867b4815b453d0
Andres Puulinn <andres_puulinn@symantec.com>
2017-04-21 11:44:57 +0300

    Look for data files in the same folder where perl script is located

--- memento.pl
+++ memento.pl
@@ -14,7 +14,11 @@ if ($#ARGV == 0 and $ARGV[0] eq '--') {
 	@ARGV = split(/\s+/);
 }
 
-my $dataFolder = '.';
+# Look for data files in the same folder where perl script is located
+my $dataFolder = $0;
+$dataFolder =~ s![^/\\]+$!!;
+$dataFolder =~ s![/\\]$!!;
+
 my $maxKeyLimit = 32;
 
 my @masks;
